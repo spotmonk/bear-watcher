749c83f5fcdffc079bcea3a3410126cd7cd12d41
Geno McNew <genemcnew3@gmail.com>
2020-05-29 06:41:05 -0500

    basic working cards

--- javascripts/river.js
+++ javascripts/river.js
@@ -5,7 +5,13 @@ const printRiver = () => {
     const bears = bearData.getBears()
     let domString = ``
     bears.forEach(bears =>
-    domString += `${bears.name}`)
+    domString += `<div class="card" style="width: 18rem;">
+    <h5 class="card-header">${bears.name}</h5>
+    <img src="${bears.imgURL}" class="card-img-top" alt="...">
+    <div class="card-body">
+      
+    </div>
+  </>`)
     
     utils.printToDom("#cards", domString)
 }
