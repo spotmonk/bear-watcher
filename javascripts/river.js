3d56f2ef37743bb3037433af61dfb67cf4f2726d
Geno McNew <genemcnew3@gmail.com>
2020-05-28 22:07:26 -0500

    wip

--- javascripts/river.js
+++ javascripts/river.js
@@ -1,1 +1,13 @@
-export default {}
+import bearData from './helpers/data/bearData.js'
+import utils from './helpers/utils.js'
+
+const printRiver = () => {
+    const bears = bearData.getBears()
+    let domString = ``
+    bears.forEach(bears =>
+    domString += `${bears.name}`)
+    
+    utils.printToDom("#cards", domString)
+}
+
+export default {printRiver}
